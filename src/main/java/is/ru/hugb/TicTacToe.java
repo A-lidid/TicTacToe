097810edb69510a6a3fbadf109b1115dbee54428
Arnar Þórðarson <arnart08@ru.is>
2017-10-16 20:33:04 +0000

    added gradle files, initial folder structure and added gradle to gitignore

---
+++ src/main/java/is/ru/hugb/TicTacToe.java
@@ -0,0 +1,10 @@
+package is.ru.hugb;
+
+public class TicTacToe{
+	
+	public static void main(String[] args) {   
+		
+	}
+
+
+}
---
+++ src/test/java/is/ru/hugb/TicTacToeTest.java
@@ -0,0 +1,9 @@
+package is.ru.hugb;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TicTacToeTest{
+
+}
