37eedc8916af19bd80d1a4a3eaf8c6ffe78e1e83
Arnar Þórðarson <arnart08@ru.is>
2017-10-18 21:59:30 +0000

    added function to return status of the board

--- src/main/java/is/ru/hugb/TicTacToe.java
+++ src/main/java/is/ru/hugb/TicTacToe.java
@@ -3,8 +3,10 @@ package is.ru.hugb;
 public class TicTacToe{
 	
 	public static void main(String[] args) {   
-		
-	}
 
+	}
+	public String returnGameBoard(){
+		return "123\n456\n789\n";
+	}
 
 }
--- src/test/java/is/ru/hugb/TicTacToeTest.java
+++ src/test/java/is/ru/hugb/TicTacToeTest.java
@@ -5,5 +5,9 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TicTacToeTest{
-
+	static TicTacToe game = new TicTacToe();
+	@Test
+	public void returnGameBoard() {
+		assertEquals("123\n456\n789\n", game.returnGameBoard());
+	}
 }
