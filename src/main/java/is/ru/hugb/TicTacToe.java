383843b46d8a819836e0698f17996675766c5112
Arnar Þórðarson <arnart08@ru.is>
2017-10-19 11:50:35 +0000

    refactored toString to build string representation from gameBoard

--- src/main/java/is/ru/hugb/TicTacToe.java
+++ src/main/java/is/ru/hugb/TicTacToe.java
@@ -1,12 +1,29 @@
 package is.ru.hugb;
 
 public class TicTacToe{
-	
+	private char[][] gameBoard;
+	private final int BOARDSIZE = 3;
+	public TicTacToe() {
+		
+		gameBoard = new char[BOARDSIZE][BOARDSIZE];
+		for(int row = 0; row < BOARDSIZE; row++) {
+			for(int col = 0; col < BOARDSIZE; col++) {
+				gameBoard[row][col] = Character.forDigit(row * 3 + col, 10);
+			}
+		}
+	}
 	public static void main(String[] args) {   
 		
 	}
 
 	public String toString() {
-		return "012\n345\n678\n";
+		StringBuilder builder = new StringBuilder();
+		for(int row = 0; row < BOARDSIZE; row++) {
+			for(int col = 0; col < BOARDSIZE; col++) {
+				builder.append(gameBoard[row][col]);
+			}
+			builder.append("\n");
+		}
+		return builder.toString();
 	}
 }
