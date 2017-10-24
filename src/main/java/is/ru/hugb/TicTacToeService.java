4daa8bf8790fb497789b012c3f973514086ba905
Arnar Þórðarson <arnart08@ru.is>
2017-10-24 15:23:36 +0000

    added functions that return the board position as an array

--- src/main/java/is/ru/hugb/TicTacToeService.java
+++ src/main/java/is/ru/hugb/TicTacToeService.java
@@ -1,5 +1,7 @@
 package is.ru.hugb;
 
+import java.util.ArrayList;
+
 public class TicTacToeService {
 	private TicTacToe ticTacToe = new TicTacToe();
 	private int turnCount = 0;
@@ -7,15 +9,21 @@ public class TicTacToeService {
 	public String toString() {
 		return ticTacToe.toString();
 	}
+
 	public void initialize() {
 		turnCount = 0;
 		ticTacToe.initializeGameboard();
 	}
+
 	public boolean insertSymbol(int number) {
 		if(ticTacToe.insertSymbol(turnCount % 2 + 1, number)) {
 			turnCount++;
 			return true;
 		}
 		return false;
+	}
+
+	public ArrayList<String> getArray() {
+		return ticTacToe.getArray();
 	}
 }
