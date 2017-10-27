1403435cdfe48785073d2b090598141aded2d960
Arnar Þórðarson <arnart08@ru.is>
2017-10-27 20:26:25 +0000

    added selenium support

---
+++ src/selenium/java/is/ru/hugb/TestTicTacToeWeb.java
@@ -0,0 +1,16 @@
+package is.ru.hugb;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.Keys;
+
+public class TestTicTacToeWeb extends SeleniumTestWrapper {
+  @Test
+  public void testTitleMatches() {
+    driver.get(baseUrl);
+    assertEquals("Tic Tac Toe", driver.getTitle());
+  }
+}
