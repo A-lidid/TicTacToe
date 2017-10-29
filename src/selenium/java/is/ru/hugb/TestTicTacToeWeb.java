ceeb341f25823e7131b4c7662bd194bb8ae65699
Arnar Þórðarson <arnart08@ru.is>
2017-10-29 15:53:45 +0000

    increased wait time between selenium tests

--- src/selenium/java/is/ru/hugb/TestTicTacToeWeb.java
+++ src/selenium/java/is/ru/hugb/TestTicTacToeWeb.java
@@ -9,43 +9,46 @@ import org.openqa.selenium.Keys;
 
 public class TestTicTacToeWeb extends SeleniumTestWrapper {
   @Test
-  public void testTitleMatches() {
+  public void testTitleMatches() throws Exception {
     driver.get(baseUrl);
+    Thread.sleep(2000);
     assertEquals("Tic Tac Toe", driver.getTitle());
   }
   @Test
   public void testSymbolAppears() throws Exception {
     driver.get(baseUrl);
+    Thread.sleep(2000);
     WebElement element = driver.findElement(By.id("-1"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("0"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(1200);
+    Thread.sleep(1000);
     assertEquals("x", element.getText());
   }
 
   @Test
   public void testXwins() throws Exception {
   	driver.get(baseUrl);
+  	Thread.sleep(2000);
   	WebElement element = driver.findElement(By.id("-1"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("0"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("3"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("1"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("4"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("2"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(1200);
+    Thread.sleep(1000);
     element = driver.findElement(By.id("status"));
     assertEquals("'X' vann!", element.getText());
   }
@@ -53,27 +56,28 @@ public class TestTicTacToeWeb extends SeleniumTestWrapper {
   @Test
   public void testOwins() throws Exception {
   	driver.get(baseUrl);
+  	Thread.sleep(2000);
   	WebElement element = driver.findElement(By.id("-1"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("1"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("0"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("2"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("4"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("5"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("8"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(1200);
+    Thread.sleep(1000);
     element = driver.findElement(By.id("status"));
     assertEquals("'O' vann!", element.getText());
   }
@@ -81,36 +85,37 @@ public class TestTicTacToeWeb extends SeleniumTestWrapper {
   @Test
   public void testEvenSteven() throws Exception {
   	driver.get(baseUrl);
+  	Thread.sleep(2000);
   	WebElement element = driver.findElement(By.id("-1"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("1"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("0"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("4"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("3"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("5"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("7"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("8"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("2"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("6"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(1200);
+    Thread.sleep(1000);
     element = driver.findElement(By.id("status"));
     assertEquals("Jafntefli!", element.getText());
   }
@@ -118,15 +123,16 @@ public class TestTicTacToeWeb extends SeleniumTestWrapper {
   @Test
   public void testNoChange() throws Exception {
   	driver.get(baseUrl);
+  	Thread.sleep(2000);
   	WebElement element = driver.findElement(By.id("-1"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("1"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(500);
+    Thread.sleep(700);
     element = driver.findElement(By.id("1"));
     element.sendKeys(Keys.RETURN);
-    Thread.sleep(1200);
+    Thread.sleep(1000);
     element = driver.findElement(By.id("1"));
     assertEquals("x", element.getText());
   }
